8e18f5ad9484bee1eaeabee71e4670d61de70a8e
undo <undo@pixiv.co.jp>
2026-05-11 16:18:50 +0900

    feat: charcoal v5に対応

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,12 @@
 import { SessionProvider } from 'next-auth/react';
 import { ThemeProvider, createGlobalStyle } from 'styled-components';
-import { light } from '@charcoal-ui/theme';
+import { TokenInjector } from '@charcoal-ui/styled';
 import { CharcoalProvider, OverlayProvider, SSRProvider } from '@charcoal-ui/react';
-import '../src/globals.css';
+import { light } from '@charcoal-ui/theme';
+import { themeMap } from '../utils/variables';
 import Head from 'next/head';
+import '@charcoal-ui/react/dist/index.css';
+import '@charcoal-ui/react/dist/layered.css';
 
 export default function App({ Component, pageProps }) {
   return (
@@ -16,13 +19,14 @@ export default function App({ Component, pageProps }) {
       </Head>
       <SessionProvider session={pageProps.session}>
         <SSRProvider>
-          <CharcoalProvider themeMap={{ ':root': light }}>
-            <OverlayProvider>
-              <ThemeProvider theme={light}>
-                <GlobalStyle />
+          <CharcoalProvider>
+            <ThemeProvider theme={light}>
+              <TokenInjector theme={themeMap} />
+              <GlobalStyle />
+              <OverlayProvider>
                 <Component {...pageProps} />
-              </ThemeProvider>
-            </OverlayProvider>
+              </OverlayProvider>
+            </ThemeProvider>
           </CharcoalProvider>
         </SSRProvider>
       </SessionProvider>
@@ -31,8 +35,8 @@ export default function App({ Component, pageProps }) {
 }
 
 const GlobalStyle = createGlobalStyle`
-body {
-  font-family: 'Noto Sans JP', sans-serif;
-  font-size: ${(props) => props.theme.typography.size[14].fontSize}px;
-}
+  body {
+    font-family: 'Noto Sans JP', sans-serif;
+    font-size: ${(props) => props.theme.typography.size[14].fontSize}px;
+  }
 `;
---
+++ utils/variables.ts
@@ -0,0 +1,20 @@
+import { light, type CharcoalTheme, type CharcoalAbstractTheme } from '@charcoal-ui/theme';
+import { themeSelector } from '@charcoal-ui/styled';
+
+// この辺はcharcoal v4以降で型エラーになるのを対処するための記述
+export type ExampleTheme = typeof light & CharcoalTheme;
+
+declare module 'styled-components' {
+  interface DefaultTheme extends ExampleTheme {}
+}
+
+export interface ThemeMap<T extends Pick<CharcoalAbstractTheme, 'color' | 'effect' | 'border'>> {
+  ':root': T;
+  [mediaQuery: `@media (${string})`]: T;
+  [selector: string]: T;
+}
+
+export const themeMap: ThemeMap<CharcoalTheme> = {
+  ':root': light,
+  [themeSelector('light')]: light,
+};
